cbd065fc53b4f0a608eaa742eb22a94f8c76ca74
Adam Holt <holtsoftware@outlook.com>
2018-09-26 19:44:59 -0600

    fixed paths in scripts.

--- build.ps1
+++ build.ps1
@@ -3,10 +3,10 @@
 if($IsLinux -eq $true)
 {
 	$env:SANNEL_ARCH=(uname -p)
-	docker-compose -f .\docker-compose.yml -f .\docker-compose.unix.yml build
+	docker-compose -f docker-compose.yml -f docker-compose.unix.yml build
 }
 else
 {
 	$env:SANNEL_ARCH="win"
-	docker-compose -f .\docker-compose.yml -f .\docker-compose.windows.yml build
+	docker-compose -f docker-compose.yml -f docker-compose.windows.yml build
 }
--- down.ps1
+++ down.ps1
@@ -4,10 +4,10 @@
 if($IsLinux -eq $true)
 {
 	$env:SANNEL_ARCH=(uname -p)
-	docker-compose -f .\docker-compose.yml -f .\docker-compose.unix.yml down
+	docker-compose -f docker-compose.yml -f docker-compose.unix.yml down
 }
 else
 {
 	$env:SANNEL_ARCH="win"
-	docker-compose -f .\docker-compose.yml -f .\docker-compose.windows.yml down
+	docker-compose -f docker-compose.yml -f docker-compose.windows.yml down
 }
