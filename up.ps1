2946670ba9969c6679d90543c8f664c02f6682ab
Adam Holt <holtsoftware@outlook.com>
2018-09-26 19:49:18 -0600

    update scripts.

--- up.ps1
+++ up.ps1
@@ -3,7 +3,8 @@
 
 if($IsLinux -eq $true)
 {
-	$env:SANNEL_ARCH=(uname -p)
+	$uname = uname -p
+	$env:SANNEL_ARCH="linux-$uname"
 	docker-compose -f docker-compose.yml -f docker-compose.unix.yml up
 }
 else
