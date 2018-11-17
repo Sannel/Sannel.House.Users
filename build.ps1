60487a3c278336f60b719a2c0f925bc83cd1a89f
Adam Holt <holtsoftware@outlook.com>
2018-11-17 15:27:34 -0700

    Updated Dockerfile to include mysql project. Updated build to auto generate version number.

--- build.ps1
+++ build.ps1
@@ -4,10 +4,12 @@ if($IsLinux -eq $true)
 {
 	$uname = uname -p
 	$env:SANNEL_ARCH="linux-$uname"
+	$env:SANNEL_VERSION=Get-Date -format yyMM.dd
 	return docker-compose -f docker-compose.yml -f docker-compose.unix.yml build
 }
 else
 {
 	$env:SANNEL_ARCH="win"
+	$env:SANNEL_VERSION=Get-Date -format yyMM.dd
 	return docker-compose -f docker-compose.yml -f docker-compose.windows.yml build
 }
