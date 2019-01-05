4ca63d0401d87b0810da1f954a1864ebd2072bfc
Adam Holt <holtsoftware@outlook.com>
2019-01-05 14:18:29 -0700

    Updated Versions to the latest

---
+++ push.ps1
@@ -0,0 +1,30 @@
+#!/usr/local/bin/pwsh
+param(
+)
+
+$target = "devices"
+
+if($IsLinux -eq $true -or $IsMacOS -eq $true)
+{
+	$uname = uname -p
+	if($uname -eq "x86_64" -or $uname -eq "i386")
+	{
+		$uname = "x86";
+	}
+	if($uname -eq "armv7l" -or $uname -eq "unknown") # for now assume unknown is arm
+	{
+		$uname = "arm";
+	}
+
+	$env:USER="root"
+	$env:SANNEL_ARCH="linux-$uname"
+	$env:SANNEL_VERSION=Get-Date -format yyMM.dd
+	return docker-compose -f docker-compose.yml -f docker-compose.unix.yml push $target
+}
+else
+{
+	$env:USER="administrator"
+	$env:SANNEL_ARCH="win"
+	$env:SANNEL_VERSION=Get-Date -format yyMM.dd
+	return docker-compose -f docker-compose.yml -f docker-compose.windows.yml push $target
+}
