09708d05cca72efe380e1b6466d25f5ad17b6dd4
Adam Holt <holtsoftware@outlook.com>
2019-01-20 15:20:40 -0700

    Added build-manifest script.

---
+++ build-manifest.ps1
@@ -0,0 +1,15 @@
+param()
+
+$version = $env:BUILD_BUILDNUMBER;
+
+if($null -eq $version -or $version -eq "")
+{
+	$version = (Get-Date -Format yyMMdd) + "-local";
+}
+
+$combinedTag="${env:DOCKER_REGISTRY}sannelhouseusers:beta-$version"
+$arm="${env:DOCKER_REGISTRY}sannelhouseusers:build-$version-linux-arm"
+$x64="${env:DOCKER_REGISTRY}sannelhouseusers:build-$version-linux-x64"
+$win="${env:DOCKER_REGISTRY}sannelhouseusers:build-$version-win"
+
+docker manifest create $combinedTag $arm $x64 $win
