a132085c01e2833d97e5c24917193223ddc17bb4
Adam Holt <holtsoftware@outlook.com>
2019-01-20 13:42:39 -0700

    Updates to image tagging convention.

--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@
 param(
 )
 
+$version = $env:BUILD_BUILDNUMBER;
 $target = "";
 
 Remove-Item -ErrorAction SilentlyContinue src/Sannel.House.Users/app_data/data.db
@@ -9,10 +10,15 @@ Remove-Item -ErrorAction SilentlyContinue src/Sannel.House.Users/app_data/data.d
 docker pull microsoft/dotnet:2.2-aspnetcore-runtime
 docker pull microsoft/dotnet:2.2-sdk
 
+if($null -eq $version -or $version -eq "")
+{
+	$version = (Get-Date -Format yyMMdd) + "-local";
+}
+
 if($IsLinux -eq $true -or $IsMacOS -eq $true)
 {
 	Write-Host "Building on Linux"
-	$uname = uname -p
+	$uname = uname -m
 	if($uname -eq "x86_64" -or $uname -eq "i386")
 	{
 		$uname = "x64";
@@ -24,7 +30,7 @@ if($IsLinux -eq $true -or $IsMacOS -eq $true)
 
 	$env:USER="root"
 	$env:SANNEL_ARCH="linux-$uname"
-	$env:SANNEL_VERSION=Get-Date -format yyMM.dd
+	$env:SANNEL_VERSION=$version
 	return docker-compose -f docker-compose.yml -f docker-compose.unix.yml build $target
 }
 else
@@ -32,6 +38,6 @@ else
 	Write-Host "Building on Windows"
 	$env:USER="administrator"
 	$env:SANNEL_ARCH="win"
-	$env:SANNEL_VERSION=Get-Date -format yyMM.dd
+	$env:SANNEL_VERSION=$version
 	return docker-compose.exe -f docker-compose.yml -f docker-compose.windows.yml build $target
 }
--- push.ps1
+++ push.ps1
@@ -2,11 +2,16 @@
 param(
 )
 
-$target = "users"
+$version = $env:BUILD_BUILDNUMBER;
+
+if($null -eq $version -or $version -eq "")
+{
+	$version = (Get-Date -Format yyMMdd) + "-local";
+}
 
 if($IsLinux -eq $true -or $IsMacOS -eq $true)
 {
-	$uname = uname -p
+	$uname = uname -m
 	if($uname -eq "x86_64" -or $uname -eq "i386")
 	{
 		$uname = "x86";
@@ -16,14 +21,12 @@ if($IsLinux -eq $true -or $IsMacOS -eq $true)
 		$uname = "arm";
 	}
 
-	$env:USER="root"
 	$env:SANNEL_ARCH="linux-$uname"
 	$env:SANNEL_VERSION=Get-Date -format yyMM.dd
 	return docker-compose -f docker-compose.yml -f docker-compose.unix.yml push $target
 }
 else
 {
-	$env:USER="administrator"
 	$env:SANNEL_ARCH="win"
 	$env:SANNEL_VERSION=Get-Date -format yyMM.dd
 	return docker-compose -f docker-compose.yml -f docker-compose.windows.yml push $target
